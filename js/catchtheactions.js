ef3dda84b52bc2570cff9fadef395cc7caa32cda
ogunsanwodavid <ogunsanwodavid123@gmail.com>
2024-03-08 21:45:42 +0100

    building catch the actions page

--- js/catchtheactions.js
+++ js/catchtheactions.js
@@ -244,3 +244,199 @@ const lifeInTheBigHouseSwiper = new Swiper('.life-in-the-big-house-swiper', {
 );
 
 /************** Script for the Life in The Big House swiper js **********/
+
+
+/************** Script for the  Next on The Show swiper js **********/
+const nextOnTheShowSwiper = new Swiper('.next-on-the-show-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+
+    // If we need pagination
+   /*  pagination: {
+        el: '.swiper-pagination',
+    }, */
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.next-on-the-show-swiper-button-next',
+        prevEl: '.next-on-the-show-swiper-button-prev',
+    }, 
+
+    //swiper speed
+    /* speed: 1000,
+
+    autoplay: false */
+
+    slidesPerView: 1,
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 480px
+        690: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+
+        },
+        // when window width is >= 640px
+        1047: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+        }
+    }
+}
+);
+
+/************** Script for the  Next on The Show swiper js **********/
+
+
+
+/************** Script for the  The Big Surprise swiper js **********/
+const theBigSurpriseSwiper = new Swiper('.the-big-surprise-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+
+    // If we need pagination
+   /*  pagination: {
+        el: '.swiper-pagination',
+    }, */
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.the-big-surprise-swiper-button-next',
+        prevEl: '.the-big-surprise-swiper-button-prev',
+    }, 
+
+    //swiper speed
+    /* speed: 1000,
+
+    autoplay: false */
+
+    slidesPerView: 1,
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 480px
+        690: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+
+        },
+        // when window width is >= 640px
+        1047: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+        }
+    }
+}
+);
+
+/************** Script for the  Next on The Show swiper js **********/
+
+
+
+/************** Script for the Today at the Big House swiper js **********/
+const todayAtTheBigHouseSwiper = new Swiper('.today-at-big-house-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+
+    // If we need pagination
+   /*  pagination: {
+        el: '.swiper-pagination',
+    }, */
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.today-at-big-house-swiper-button-next',
+        prevEl: '.today-at-big-house-swiper-button-prev',
+    }, 
+
+    //swiper speed
+    /* speed: 1000,
+
+    autoplay: false */
+
+    slidesPerView: 1,
+
+    // Responsive breakpoints
+    breakpoints: {
+        // when window width is >= 480px
+        690: {
+        slidesPerView: 2,
+        spaceBetween: 24,
+
+        },
+        // when window width is >= 640px
+        1047: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+        }
+    }
+}
+);
+
+/************** Script for the  Next on The Show swiper js **********/
+
+
+
+/********** Script for the Video showcase Modal functionality */
+const videoShowcaseModal = document.getElementById("video-showcase-modal")
+const videoShowcaseMain = document.getElementById("video-showcase-main")
+const videoShowcaseIframe = document.getElementById("video-showcase-iframe")
+const videoShowcaseTitle = document.getElementById("video-showcase-title")
+const videoShowcaseDate = document.getElementById("video-showcase-date")
+const videoShowcaseDetails = document.getElementById("video-showcase-details")
+const closeVideoShowcaseIcon = document.getElementById("close-video-showcase-icon")
+var videoContents = document.querySelectorAll('.video-content');
+var videoTitles = document.querySelectorAll('.video-title');
+var videoDetails = document.querySelectorAll('.video-details');
+
+//show video showcase modal
+function showVideoShowcaseModal() {
+    videoShowcaseModal.classList.remove("hidden")
+    videoShowcaseModal.classList.add("flex")
+}
+
+//close video showcase modal
+function closeVideoShowcaseModal() {
+    videoShowcaseModal.classList.remove("flex")
+    videoShowcaseModal.classList.add("hidden")
+}
+
+//close video showcase icon functionality
+closeVideoShowcaseIcon.addEventListener("click" , ()=> {
+    closeVideoShowcaseModal()
+})
+
+////this closes the modal if anywhere outside the main part of the modal has been clicked.
+videoShowcaseModal.addEventListener("click" , (event)=> {
+    var isClickedInside = videoShowcaseMain.contains(event.target);
+
+    // If the clicked element is outside the div, log a message
+    if (!isClickedInside) {
+      closeVideoShowcaseModal()
+    }
+})
+
+// Add click event listener to each "video-content" element
+for (let i = 0; i < videoContents.length; i++) {
+    videoContents[i].addEventListener("click" , ()=> {
+        //show video showcase
+        showVideoShowcaseModal();
+
+        //change video showcase title to clicked video
+        videoShowcaseTitle.innerHTML = videoTitles[i].textContent
+
+        //change video showcase details to clicked video
+        videoShowcaseDetails.innerHTML = videoDetails[i].textContent
+
+        //change video showcase iframe source to clicked video
+        videoShowcaseIframe.src = videoContents[i].getAttribute("iframe-src")
+
+        //change video showcase date to clicked video
+        videoShowcaseDate.innerHTML = videoContents[i].getAttribute("video-date")
+    })
+}
+
+
+
+/********** Script for the Video showcase Modal functionality */
